283369d39ddf325fce58706d934f60c326286cf7
Hamid <hamid.hessamei@gmail.com>
2020-02-27 00:42:31 +0330

    Updates

---
+++ precache-manifest.ff1705508da7b18fe57f4a4b7f019c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5922abf01305b215c234343c48b1b746",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "81d79411d69370b72840",
+    "url": "/maktabkhoone-resume/static/css/main.9185972e.chunk.css"
+  },
+  {
+    "revision": "a9aa75006f951eebea03",
+    "url": "/maktabkhoone-resume/static/js/2.e3f927f7.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/maktabkhoone-resume/static/js/2.e3f927f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d79411d69370b72840",
+    "url": "/maktabkhoone-resume/static/js/main.2c659044.chunk.js"
+  },
+  {
+    "revision": "d03aafdb2cff06a04b0b",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.44529cb9.js"
+  }
+]);
